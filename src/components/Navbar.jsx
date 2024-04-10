70aa6339d544dccef86b30fe1859200d105ce43c
abhishek <abhishekpl8219@gmail.com>
2024-04-10 19:36:56 +0530

    Creating a basic navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import Link from 'next/link';
 export default function Navbar() {
   return (
-    <div>
+    <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
       Navbar
+      <div>
+        <Link href="/" className='text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center'></Link>
+        <span className='mr-1'> 
+         Rappy
+        </span>
+        <span className='w-12 h-8 rounded bg-white text-black flex items-center'> .Dev</span>
+      </div>
     </div>
   )
 }
